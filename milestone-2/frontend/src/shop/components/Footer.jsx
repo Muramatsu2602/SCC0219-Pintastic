d401671eb33d985d1a1371a3befc9b1481c0190d
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2023-05-20 19:57:08 -0300

    Footer is ready

---
+++ milestone-2/frontend/src/shop/components/Footer.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import './Footer.style.css';
+import VisaImg from './assets/img/logos/visa.png';
+import MastercardImg from './assets/img/logos/mastercard.png';
+import PixImg from "./assets/img/logos/pix.png"
+import BoletoImg from "./assets/img/logos/boleto.png"
+import LogoImg from "./assets/img/pintastic-logo.png"
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-main">
+        <div className="footer-left">
+          <div className="footer-column">
+            <h4>Social Media</h4>
+            <ul>
+              <li><a href="#">Facebook</a></li>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Instagram</a></li>
+              <li><a href="#">YouTube</a></li>
+              <li><a href="#">LinkedIn</a></li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <h4>Contact</h4>
+            <ul>
+              <li><a href="#">Talk to Us</a></li>
+              <li><a href="#">FAQ</a></li>
+            </ul>
+          </div>
+          <div className="footer-column">
+            <h4>Pintastic</h4>
+            <ul>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Address</a></li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-right">
+          <div className="payment-icons">
+            <img src={VisaImg} alt="Visa" />
+            <img src={MastercardImg} alt="Mastercard" />
+            <img src={PixImg} alt="PIX" />
+            <img src={BoletoImg} alt="Boleto" />
+          </div>
+        </div>
+      </div>
+      <div className="footer-under">
+        <div className="footer-logo">
+          <img src={LogoImg} alt="Logo" />
+        </div>
+        <div>
+          <span>© 2023, All rights reserved</span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- milestone-2/frontend/src/shop/pages/LandingPage.jsx
+++ milestone-2/frontend/src/shop/pages/LandingPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Button from '../components/Button.jsx';
+import Footer from '../components/Footer.jsx';
+
 
 export default function LandingPage() {
   return (
     <>
       Landing page
       <Button buttonText={"Teste"}/>
+      <Footer/>
     </>
   );
 };
