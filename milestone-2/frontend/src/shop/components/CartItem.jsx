f806de1f88a12f74a628563589138e1f84830f83
Pedro Muramatsu <pedromuramatsuc@gmail.com>
2023-06-03 16:40:45 -0300

    Cart item delete, alter quantity & empty cart are done

---
+++ milestone-2/frontend/src/shop/components/CartItem.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const CartItem = ({ item, removeItem }) => {
+  const [quantity, setQuantity] = useState(item.quantity);
+
+  const handleQuantityChange = (event) => {
+    const newQuantity = parseInt(event.target.value);
+    setQuantity(newQuantity);
+  };
+
+  return (
+    <div className='cart-item'>
+      <img
+        src={item.productImage}
+        alt='cart item product'
+        className='cart-image'
+      />
+      <div className='cart-details'>
+        <h3>{item.productTitle}</h3>
+        <p>{item.productDescription}</p>
+        <div className='cart-actions'>
+          <div>
+            <label htmlFor='quantity'>Quantity:</label>
+            <input
+              type='number'
+              name='quantity'
+              id='quantity'
+              min='1'
+              max='10'
+              value={quantity}
+              onChange={handleQuantityChange}
+            />
+          </div>
+          <div className='cart-remove'>
+            <button type='button' onClick={() => removeItem(item.id)}>
+              <FontAwesomeIcon icon={faTrashAlt} />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className='cart-price'>
+        <h3>${(item.productPrice * quantity).toFixed(2)}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
--- milestone-2/frontend/src/shop/components/CartSection.jsx
+++ milestone-2/frontend/src/shop/components/CartSection.jsx
@@ -1,10 +1,10 @@
-import './CartSection.style.css'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import './CartSection.style.css'
+import CartItem from './CartItem'
 
 const CartSection = () => {
-  // Mock data for cart items
-  const cartItems = [
+  const [cartItems, setCartItems] = useState([
     {
       id: 1,
       productTitle: 'Product Title 1',
@@ -33,7 +33,7 @@ const CartSection = () => {
       quantity: 2
     }
     // Add more mock data items as needed
-  ]
+  ])
 
   // Calculate the total price of items in the cart
   const totalPrice = cartItems.reduce(
@@ -41,65 +41,50 @@ const CartSection = () => {
     0
   )
 
+  // Remove an item from the cart
+  const removeItem = itemId => {
+    const updatedCartItems = cartItems.filter(item => item.id !== itemId)
+    setCartItems(updatedCartItems)
+  }
+
+  // Check if the cart is empty
+  const isCartEmpty = cartItems.length === 0
+
   return (
     <section className='cart-section'>
       <h2>Shopping Cart</h2>
-      {cartItems.map(item => (
-        <CartItem key={item.id} item={item} />
-      ))}
-      <div className='cart-total'>
-        <h2>Total:</h2>
-        <h2>${totalPrice.toFixed(2)}</h2>
-      </div>
-      <div className='cart-actions'>
-        <Link to='/'>
-          <button className='pintastic-button' type='button'>
-            Continue Shopping
-          </button>
-        </Link>
-
-        <button className='pintastic-button' type='button'>
-          Checkout
-        </button>
-      </div>
-    </section>
-  )
-}
-
-const CartItem = ({ item }) => {
-  return (
-    <div className='cart-item'>
-      <img
-        src={item.productImage}
-        alt='cart item product'
-        className='cart-image'
-      />
-      <div className='cart-details'>
-        <h3>{item.productTitle}</h3>
-        <p>{item.productDescription}</p>
-        <div className='cart-actions'>
-          <div>
-            <label htmlFor='quantity'>Quantity:</label>
-            <input
-              type='number'
-              name='quantity'
-              id='quantity'
-              min='1'
-              max='10'
-              value={item.quantity}
-            />
+      {isCartEmpty ? (
+        <div className='cart-empty'>
+          <h1>Your cart is empty</h1>
+          <Link to='/'>
+            <button className='pintastic-button' type='button'>
+              Continue Shopping
+            </button>
+          </Link>
+        </div>
+      ) : (
+        <>
+          {cartItems.map(item => (
+            <CartItem key={item.id} item={item} removeItem={removeItem} />
+          ))}
+          <div className='cart-total'>
+            <h2>Total:</h2>
+            <h2>${totalPrice.toFixed(2)}</h2>
           </div>
-          <div className='cart-remove'>
-            <button type='button'>
-              <i className='fas fa-trash-alt'></i> Remove
+          <div className='cart-actions'>
+            <Link to='/'>
+              <button className='pintastic-button' type='button'>
+                Continue Shopping
+              </button>
+            </Link>
+
+            <button className='pintastic-button' type='button'>
+              Checkout
             </button>
           </div>
-        </div>
-      </div>
-      <div className='cart-price'>
-        <h3>${item.productPrice.toFixed(2)}</h3>
-      </div>
-    </div>
+        </>
+      )}
+    </section>
   )
 }
 
