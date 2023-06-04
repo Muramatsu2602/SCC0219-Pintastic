9dabea2e0b7a84ee391ab0429945396b7635f21a
Andre Santana <andre_sfnd@usp.br>
2023-06-04 17:15:18 -0300

    Add logout on admin panel

--- milestone-2/frontend/src/admin/components/Sidebar/index.jsx
+++ milestone-2/frontend/src/admin/components/Sidebar/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import {useAuth} from '../../../contexts/Auth';
+
 export default styled(RawSidebar)`
   min-width: 250px;
   height: calc(100vh - 80px);
@@ -31,6 +33,16 @@ export default styled(RawSidebar)`
 `;
 
 function RawSidebar(props) {
+  const auth = useAuth();
+
+  async function handleLogout(e) {
+    try {
+      await auth.logout();
+    } catch (error) {
+      alert('Não foi possível sair do sistema, tente novamente mais tarde');
+    }
+  }
+
   return (
     <div className={props.className}>
       <ul className="navigation-buttons">
@@ -38,7 +50,7 @@ function RawSidebar(props) {
         <a href=""><li><i className="fa fa-list"></i> Produtos</li></a>
         <a href=""><li><i className="fa fa-group"></i> Clientes</li></a>
         <a href=""><li><i className="fa fa-lock"></i> Administradores</li></a>
-        <a href=""><li><i className="fa fa-sign-out"></i> Sair</li></a>
+        <a onClick={handleLogout}><li><i className="fa fa-sign-out"></i> Sair</li></a>
       </ul>
     </div>
   );
