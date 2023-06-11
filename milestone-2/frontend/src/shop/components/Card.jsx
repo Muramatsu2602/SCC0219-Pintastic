a8de205be9228965f1969e17afb734901e09f8f8
Andre Santana <andre_sfnd@usp.br>
2023-06-11 13:41:44 -0300

    Fix pin of the day discount NaN bug

--- milestone-2/frontend/src/shop/components/Card.jsx
+++ milestone-2/frontend/src/shop/components/Card.jsx
@@ -39,6 +39,7 @@ const Card = ({
         productPrice,
         productTitle,
         productDescription,
+        productDiscountPercentage,
         productImage,
         productRating,
         productCategory,
