8fd42ab5dfd534798ee78255b4a00c7f7fa80d63
Ana Oliveira <ana.oliveira@bemobi.com>
2023-06-22 22:01:55 -0300

    Add https requests

--- milestone-3/server/server.js
+++ milestone-3/server/server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+
+import express from 'express'
+import * as productsController from "./controllers/productsController"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
 
 // Import routes
 const productsRoutes = require('./server/routes/products');
@@ -8,27 +10,48 @@ const productsRoutes = require('./server/routes/products');
 // Create Express app
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Connect to MongoDB
-const dbURI = 'mongodb://localhost:27017/mydatabase'; // Replace with your MongoDB connection string
-mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('Failed to connect to MongoDB', err);
-    process.exit(1);
-  });
-
-// Routes
-app.use('/api/products', productsRoutes);
-
 // Start the server
 const port = process.env.PORT || 3000;
+
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb://localhost/ExpressDB",{
+  useNewUrlParser : true,
+  useUnifiedTopology : true,
+})
+
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(bodyParser.json())
+
+app.route("/student").get((req,res,next) => {
+  console.log(`Request from : ${req.originalUrl}`)
+  console.log(`Request type : ${req.method}`)
+  next();
+},productsController.getAllProducts).post(productsController.addProduct)
+
+app.route("/product/:productId")
+.get(productsController.getProductById)
+.put(productsController.updateProductById)
+.delete(productsController.deleteProductById)
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Connect to MongoDB
+// const dbURI = 'mongodb://localhost:27017/mydatabase'; // Replace with your MongoDB connection string
+// mongoose
+//   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((err) => {
+//     console.error('Failed to connect to MongoDB', err);
+//     process.exit(1);
+//   });
+
+// Routes
+// app.use('/api/products', productsRoutes);
+
