efddf4a035d9b1bc755aafc40e9907edac551de0
Ana Oliveira <ana.oliveira@bemobi.com>
2023-07-01 15:38:16 -0300

    Add product traits

--- milestone-3/server/models/schemas/Product.js
+++ milestone-3/server/models/schemas/Product.js
@@ -6,60 +6,60 @@ const productSchema = new mongoose.Schema({
     required: true,
   },
 
-  // productCreatedAt: {
-  //   type: Date,
-  //   default: Date.now(),
-  // },
+  productCreatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
 
-  // productUpdatedAt: {
-  //   type: Date,
-  //   default: Date.now(),
-  // },
+  productUpdatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
 
-  // productDeletedAt: {
-  //   type: Date,
-  //   default: Date.now(),
-  // },
+  productDeletedAt: {
+    type: Date,
+    default: null,
+  },
 
-  // productStatus: {
-  //   type: String,
-  //   required: true,
-  // },
+  productStatus: {
+    type: Boolean,
+    default: true,
+  },
 
   title: {
     type: String,
     required: true,
   },
 
-  // description: {
-  //   type: String,
-  //   required: true,
-  // },
+  description: {
+    type: String,
+    required: true,
+  },
 
-  // price: {
-  //   type: Number,
-  //   required: true,
-  // },
+  price: {
+    type: Number,
+    required: true,
+  },
 
-  // productDiscountPercentage: {
-  //   type: Number,
-  //   required: true,
-  // },
+  productDiscountPercentage: {
+    type: Number,
+    default: 0.00,
+  },
 
-  // productImage: {
-  //   type: String,
-  //   required: true,
-  // },
+  productImage: {
+    type: String,
+    required: true,
+  },
 
-  // productCategory: {
-  //   type: String,
-  //   required: true,
-  // },
+  productCategory: {
+    type: String,
+    required: true,
+  },
 
-  // productStock: {
-  //   type: Number,
-  //   required: true,
-  // },
+  productStock: {
+    type: Number,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('Product', productSchema);
