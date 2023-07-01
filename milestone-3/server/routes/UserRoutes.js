55d31451be20e062996792ed45616d735e58b52d
Ana Oliveira <ana.oliveira@bemobi.com>
2023-07-01 15:39:10 -0300

    Add user routes

---
+++ milestone-3/server/routes/UserRoutes.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+
+const UserController = require('../controllers/UserController');
+
+router.get('/user', async function(request, response, next) {
+  try {
+    const users = await UserController.getAllUsers();
+
+    return response.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/user/:id', async function(request, response, next) {
+  try {
+    const { id } = request.params;
+
+    const user = await UserController.getUserById(id);
+
+    return response.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/user', async function(request, response, next) {
+  try {
+    const { id, email } = request.body;
+
+    const user = await UserController.create(id, email);
+
+    return response.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put('/user/:id', async function(request, response, next) {
+  try {
+    const { id } = request.params;
+    const { email } = request.body;
+
+    const user = await UserController.updateById(id, email);
+
+    return response.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/user/:id', async function(request, response, next) {
+  try {
+    const { id } = request.params;
+
+    await UserController.delete(id);
+
+    return response.status(200).end();
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
